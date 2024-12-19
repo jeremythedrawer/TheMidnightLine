63dea326fcca38996e47a4d1f507f55b420efb52
jeremythedrawer <jedwards184@outlook.com>
2024-12-19 19:54:33 +1100

    import henchman kick type animations and sprites

--- Assets/Scripts/Movement System/Behaviour Scripts/Agent States/AttackState.cs
+++ Assets/Scripts/Movement System/Behaviour Scripts/Agent States/AttackState.cs
@@ -4,11 +4,17 @@ public class AttackState : State
 {
     //parent state
     public AgentMovement agentMovement;
+
+    //child states
+    public GroundState groundState;
+    public AirborneState airborneState;
     public override void Enter()
     {
     }
     public override void Do()
     {
+        SelectState();
+
         if (agentMovement.isHiding)
         {
             isComplete = true;
@@ -19,5 +25,20 @@ public class AttackState : State
     }
     public override void Exit()
     {
+    }
+
+    private void SelectState()
+    {
+        if (state.isComplete)
+        {
+            if (collisionChecker.grounded)
+            {
+                Set(groundState, true);
+            }
+            else
+            {
+                Set(airborneState, true);
+            }
+        }
     }
 }
